47d0532a3982f4df51499f5bbf984d80cbbe31e9
Chris Stubben <chris.stubben@gmail.com>
2012-02-06 11:46:33 -0700

    stoch.proj update

--- R/stoch.projection.R
+++ R/stoch.projection.R
@@ -1,32 +1,28 @@
-stoch.projection<-function(matrices, n0, tmax=50, nreps=5000, prob=NULL, nmax=NULL, sumweight=NULL, verbose=TRUE)
+stoch.projection<-function(matrices, n0, tmax=50, nreps=5000, prob=NULL, nmax=NULL, sumweight=rep(1,length(n0)), verbose=FALSE)
 {
-   if(is.list(matrices)){matrices<-matrix(unlist(matrices), ncol=length(matrices))}
-   ## initialize empty matrix to store results at each time step...
-   x<-length(n0)
-   if(is.null(sumweight)){sumweight=rep(1,x)}
-   est<-matrix(numeric(nreps*x), nrow=nreps)
-   colnames(est)<-names(n0)
-   y<-dim(matrices)[2]
+   if(!is.list(matrices)){ stop("Please use a list of matrices as input") }   
+  ## initialize empty matrix to store results at each time step...
+   est <- matrix(numeric(nreps * length(n0)), nrow = nreps)
+   colnames(est) <- names(n0)
+
    for(i in 1:nreps)
    {
-      n<-n0
-      if(verbose)
-      {
+      # random sample of matrices
+      A <- sample(matrices, tmax, replace=TRUE, prob=prob)
+      n <- n0
+      if(verbose){
          if(i==1 || i %% 100 == 0){print(paste("Starting projections for nrep", i), quote=FALSE)}
       }
-      for( j in 1:tmax)
-      {
-         col<-sample(1:y, 1, prob= prob)
-         A<-matrix(matrices[,col], nrow=x )
-         n<-A %*% n
+      for( j in 1:tmax){         
+         n <- A[[j]] %*% n
          ## simple density dependence
-         if(!is.null(nmax))
-         {
-            if(sum(n*sumweight) > nmax){n<- n * (nmax/sum(n * sumweight))}
+         if(!is.null(nmax)){
+            if(sum(n*sumweight) > nmax)  n<- n * (nmax/sum(n * sumweight)) 
          }
      }
      est[i,]<-n
   }
   est
 }
+
 
